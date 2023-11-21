a9401bb6acc26adbf718ddb1b9a80222d0a2b959
nikita-koval-skii <kn@rsdigital.ru>
2023-11-21 17:28:29 +0300

    error boundary loader

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+
+    const throwErrorBug = () => {
+        setError(true);
+    };
+
+    useEffect(() => {
+        if (!error) return;
+        throw new Error();
+    }, [error]);
+
+    return (
+        <Button
+            onClick={throwErrorBug}
+        >
+            {t('созадть ошибку')}
+        </Button>
+    );
+};
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,24 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-                <Route
-                    key={path}
-                    element={(
+    <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+            <Route
+                key={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
                         <div className="page-wrapper">
                             {element}
                         </div>
-                    )}
-                    path={path}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+                    </Suspense>
+                )}
+                path={path}
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
